63de7a3e0abe2c025b145f950890e78f9bc6dc31
Andrew <linmyintmoh@gmail.com>
2022-05-03 11:46:32 +0630

    fourth commit with login.php file update

--- login.php
+++ login.php
@@ -7,31 +7,29 @@
 		<div class="content-top" style="min-height:300px;padding:50px">
 			<div class="col-md-4 col-md-offset-4">
 				<div class="panel panel-default">
-				  <div class="panel-heading">Login</div>
-				  <div class="panel-body">
-				  	<?php //include('msgbox.php');?>
-				<p class="login-box-msg">Sign in to start your session</p>
-				<form action="loginprocess.php" method="post">
-      <div class="form-group has-feedback">
-        <input name="Email" type="text" size="25" placeholder="Email" class="form-control" placeholder="Email"/>
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input name="Password" type="password" size="25" placeholder="Password" class="form-control" placeholder="Password" />
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="form-group">
-          <button type="submit" class="btn btn-primary" name="Login">Login</button>
- 
-          <p class="login-box-msg" style="padding-top:20px">New Here? <a href="register.php">Register</a></p>
-      </div>
-      </div>
-</div>
-    </form>
+				    <div class="panel-heading">Login</div>
+				    <div class="panel-body">
+				  	    <?php //include('msgbox.php');?>
+				        <p class="login-box-msg">Sign in to start your session</p>
+				        <form action="loginprocess.php" method="post">
+                            <div class="form-group has-feedback">
+                                <input name="Email" type="text" size="25" placeholder="Email" class="form-control" placeholder="Email"/>
+                                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                            </div>
+                            <div class="form-group has-feedback">
+                                <input name="Password" type="password" size="25" placeholder="Password" class="form-control" placeholder="Password" />
+                                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary" name="Login">Login</button>
+                                <p class="login-box-msg" style="padding-top:20px">New Here? <a href="register.php">Register</a></p>
+                            </div>
+                    </div>
+                </div>
+                        </form>
 			</div>
 		</div>
-		<div class="clear"></div>	
-		
+		<div class="clear"></div>		
 	</div>
-<?php //include('footer.php');?>
+    <?php //include('footer.php');?>
 </div>
